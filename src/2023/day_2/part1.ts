10a9de0bcbce7755497c66b96804d24e476027a0
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-12-02 22:50:35 -0800

    Day 2, part 1 working

--- src/2023/day_2/part1.ts
+++ src/2023/day_2/part1.ts
@@ -1,7 +1,48 @@
 import { parseInput } from "../../utils";
 
-const input = parseInput();
+interface CountTracker {
+  red: number;
+  green: number;
+  blue: number;
+}
 
-function main() {}
+type colorType = "red" | "green" | "blue";
+
+const input = parseInput();
+const RED_MAX = 12;
+const GREEN_MAX = 13;
+const BLUE_MAX = 14;
+const MAX_COUNTS: CountTracker = {
+  red: RED_MAX,
+  green: GREEN_MAX,
+  blue: BLUE_MAX,
+};
+
+function main() {
+  let sum = 0;
+  input.forEach((line) => {
+    const [gameInfo, allRounds] = line.split(":");
+    const game = gameInfo.split(" ")[1];
+    const rounds = allRounds.split(";");
+    let validGame = true;
+    for (const round of rounds) {
+      const values = round.split(",");
+      for (const val of values) {
+        const [count, color] = val.trim().split(" ");
+        // @ts-ignore
+        if (count > MAX_COUNTS[color]) {
+          validGame = false;
+          break;
+        }
+      }
+    }
+    if (validGame) {
+      // @ts-ignore
+      sum += Number(game);
+      console.log("everything seems valid, sum is now:", sum);
+    }
+  });
+  return sum;
+}
 
 export default main();
--- src/2023/day_2/part2.ts
+++ src/2023/day_2/part2.ts
@@ -2,6 +2,10 @@ import { parseInput } from "../../utils";
 
 const input = parseInput();
 
-function main() {}
+function main() {
+  input.forEach((line) => {
+    console.log(line);
+  });
+}
 
 export default main();
