d6b2fe74911ccec4430eebf011d96a85e2565781
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-12-02 22:59:16 -0800

    Day 2 part 2 working

--- src/2023/day_2/part2.ts
+++ src/2023/day_2/part2.ts
@@ -1,11 +1,38 @@
 import { parseInput } from "../../utils";
 
+interface CountTracker {
+  red: number;
+  green: number;
+  blue: number;
+  [key: string]: number;
+}
+
 const input = parseInput();
 
 function main() {
+  let sum = 0;
   input.forEach((line) => {
-    console.log(line);
+    const [gameInfo, allRounds] = line.split(":");
+    const rounds = allRounds.split(";");
+    let mins: CountTracker = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+    for (const round of rounds) {
+      const values = round.split(",");
+      for (const val of values) {
+        const [count, color] = val.trim().split(" ");
+        if (Number(count) > mins[color]) {
+          mins[color] = Number(count);
+        }
+      }
+    }
+    const { red, green, blue } = mins;
+    const power = red * green * blue;
+    sum += power;
   });
+  return sum;
 }
 
 export default main();
